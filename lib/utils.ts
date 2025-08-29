6cd5c093f4156fd014b68e7746025ff9ca327790
narenjenni <159750555+narenjenni@users.noreply.github.com>
2025-08-29 17:59:19 +0700

    Delete lib directory

--- lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export function formatIDR(amount: number) {
-  return new Intl.NumberFormat("id-ID", {
-    style: "currency",
-    currency: "IDR",
-    maximumFractionDigits: 0
-  }).format(amount);
-}
